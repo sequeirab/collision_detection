265167afb536a6ae0a1aafc008787302c4d3c4e0
Bruno Sequeira <sequeirab86@gmail.com>
2022-08-05 17:05:55 +0100

    finished

---
+++ script.js
@@ -0,0 +1,43 @@
+/**@type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 500;
+const CANVAS_HEIGHT = canvas.height = 700;
+const explosions = [];
+
+class Explosion {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.spriteWidth = 200;
+        this.spriteHeight = 179;
+        this.width = this.spriteWidth / 2;
+        this.height = this.spriteHeight / 2;
+        this.image = new Image();
+        this.image.src = 'boom.png';
+        this.frame = 0;
+
+    }
+    update() {
+        if(this.frame === 5) {
+            this.frame = 0;
+        }
+        this.frame++;
+    }
+    draw() {
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, 0, 0, this.x, this.y, this.width, this.height)
+    }
+
+}
+
+window.addEventListener('click', (e) => {
+    ctx.fillRect(e.clientX, e.clientY, e.clientX + 100, e.clientY + 100);
+    ctx.drawImage(this.image, this.x, this.y, this.width * this.frame, this.height * this.frame, this.CANVAS_WIDTH, this.CANVAS_HEIGHT, this.image.src);
+
+})
+
+windows.addEventListener('load', () => {
+    const explo = new Explosion(x, y);
+    explo.draw();
+    explo.update();
+})
