6fb2e30999daa54590b5889825c28f581e7c5016
Bruno Sequeira <sequeirab86@gmail.com>
2022-08-08 11:50:46 +0100

    working effect

--- script.js
+++ script.js
@@ -8,29 +8,67 @@ let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
         this.spriteWidth = 200;
         this.spriteHeight = 179;
-        this.width = this.spriteWidth / 2;
-        this.height = this.spriteHeight / 2;
+        this.width = this.spriteWidth * 0.7;
+        this.height = this.spriteHeight * 0.7;
+        this.x = x;
+        this.y = y;
         this.image = new Image();
         this.image.src = 'boom.png';
         this.frame = 0;
-
+        this.timer = 0;
+        this.angle = Math.random() * 6.2;
+        this.sound = new Audio();
+        this.sound.src = 'boom.wav';
     }
     update() {
+        if(this.frame === 0) this.sound.play();
+        this.timer++;
+        if(this.timer % 10 === 0) {
+            this.frame++;
+        }
         
-        this.frame++;
     }
     draw() {
-        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, 0, 0, this.x, this.y, this.width, this.height)
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, 0 - this.width / 2, 0 - this.height / 2, this.width, this.height);
+        ctx.restore();
     }
 
 }
+const createAnimation = e => {
+    const positionX = e.x - canvasPosition.left;
+    const positionY = e.y - canvasPosition.top;
+    
+    explosions.push(new Explosion(positionX, positionY));
+}
 
 window.addEventListener('click', function(e){
-    ctx.fillStyle = 'white';
-    ctx.fillRect(e.x - canvasPosition.left, e.y - canvasPosition.top, 50, 50);
-
+    createAnimation(e);    
 })
+
+/*window.addEventListener('mousemove', function(e){
+    createAnimation(e);    
+})*/
+
+
+
+const animate = () => {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    for(let i = 0; i < explosions.length; i++) {
+        explosions[i].update();
+        explosions[i].draw();
+        if(explosions[i].frame > 5) {
+            explosions.splice(i, 1);
+            i--;
+        }
+    }
+    
+    requestAnimationFrame(animate);
+}
+
+
+animate();
